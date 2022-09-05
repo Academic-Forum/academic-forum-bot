a5fdefc8c6c76577641c7e77eb8cf19eef59ab50
[object Object] <object-object@outlook.com>
2022-09-05 11:53:54 -0700

    Make doxx command funnier

--- src/commands/doxx.rs
+++ src/commands/doxx.rs
@@ -1,8 +1,26 @@
 use crate::{BlankResult, Context};
+use poise::serenity_prelude::User;
+use rand::{thread_rng, Rng};
 
-/// Leak Ved's IP
+/// Leak someone's IP using advanced Kali Linux hacking skills
 #[poise::command(slash_command)]
-pub async fn doxx(ctx: Context<'_>) -> BlankResult {
-	ctx.say("Ved's IP is `127.0.0.1`").await?;
+pub async fn doxx(ctx: Context<'_>, #[description = "Person to doxx"] user: User) -> BlankResult {
+	let ip = {
+		let mut rng = thread_rng();
+		format!(
+			"{}.{}.{}.{}",
+			rng.gen_range(0..255),
+			rng.gen_range(0..255),
+			rng.gen_range(0..255),
+			rng.gen_range(0..255)
+		)
+	};
+
+	ctx.send(|m| {
+		m.content(format!("{}'s IP is {ip}", user))
+			.allowed_mentions(|am| am.empty_users())
+	})
+	.await?;
+
 	Ok(())
 }
